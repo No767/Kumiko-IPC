3b381a080b6dc7ab4556c3488930070cf08b0cc1
No767 <73260931+No767@users.noreply.github.com>
2022-11-19 00:23:41 -0800

    Add RabbitMQ Routed Consumers + Publishers

--- pika-server/publisher.py
+++ pika-server/publisher.py
@@ -1,25 +1,35 @@
 import asyncio
-import sys
+import os
 
 from aio_pika import DeliveryMode, ExchangeType, Message, connect
+from dotenv import load_dotenv
+
+load_dotenv()
+
+RABBITMQ_USERNAME = os.getenv("RabbitMQ_Username")
+RABBITMQ_PASSWORD = os.getenv("RabbitMQ_Password")
+RABBITMQ_HOST = os.getenv("RabbitMQ_Host")
+CONNECTION_URI = f"amqp://{RABBITMQ_USERNAME}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}/"
 
 
 async def main() -> None:
     # Perform connection
-    connection = await connect("")
+    connection = await connect(CONNECTION_URI)
 
     async with connection:
         # Creating a channel
         channel = await connection.channel()
 
         logs_exchange = await channel.declare_exchange(
-            "logs",
-            ExchangeType.FANOUT,
+            "logs_main",
+            ExchangeType.DIRECT,
         )
 
-        message_body = (
-            b" ".join(arg.encode() for arg in sys.argv[1:]) or b"Hello World!"
-        )
+        # message_body = (
+        #     b" ".join(arg.encode() for arg in sys.argv[1:]) or b"get_user_data:454357482102587393"
+        # )
+
+        message_body = b"get_user_data:454357482102587393"
 
         message = Message(
             message_body,
