5a69cd13f6b0c8f6b32798f84bf355a7a574ea0c
No767 <73260931+No767@users.noreply.github.com>
2022-12-16 19:42:39 -0800

    hoepfully one last fix

--- Docker/start.sh
+++ Docker/start.sh
@@ -38,7 +38,7 @@ fi
 
 delay=1
 while true; do
-  dramatiq-gevent Kumiko-IPC.dramatiq_ipc.worker -p 2
+  dramatiq-gevent dramatiq_ipc.worker -p 2
   if [ $? -eq 3 ]; then
     echo "Connection error encountered on startup. Retrying in $delay second(s)..."
     sleep $delay
@@ -48,4 +48,4 @@ while true; do
   fi
 done
 
-dramatiq-gevent Kumiko-IPC.dramatiq_ipc.worker -p 2
+dramatiq-gevent dramatiq_ipc.worker -p 2
